d4817d03423a153431b35a92c05fe9f814149b2c
Jared Porter <jared.r.porter@gmail.com>
2014-04-17 18:46:19 +0100

    Fixed replacement problem

--- pricegetter2.py
+++ pricegetter2.py
@@ -6,18 +6,17 @@ class Task():
     def __init__(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(3)
-        self.base_url = "http://www.home.co.uk/guides/sold_house_prices.htm?location=e10&month=01&year=2002"
+        self.base_url = "http://www.home.co.uk/guides/sold_house_prices.htm?location=%s&month=01&year=2002"
 
     def ave_price(self):
         driver = self.driver
         price_list = open("Prices.txt", "w")
         for i in postcodes:
-            self.base_url.replace("e10", i)
-            driver.get(self.base_url)
+            driver.get(self.base_url % i)
             price = driver.find_element_by_css_selector(".homeco_pr_content > div:nth-child(4) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)")
             num_sold = driver.find_element_by_css_selector(".homeco_pr_content > div:nth-child(4) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)")
             limit = driver.find_element_by_css_selector(".homeco_pr_content > h2:nth-child(3)")
-            while limit.text != "Summary of Properties Sold in %s in January 2014" % (i):
+            while limit.text != "Summary of Properties Sold in %s in January 2014" % i:
                 price_list.write(price.text.replace(u"\xa3", "") + ",")
                 price_list.write(num_sold.text + "\n")
                 driver.find_element_by_css_selector(".homeco_pr_content > div:nth-child(19) > form:nth-child(1) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(4) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > a:nth-child(1)").click()
